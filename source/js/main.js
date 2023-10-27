e399cad32e458b560ca841e187e3009269aa0e90
tayga2410 <moldazhanov.eldar@gmail.com>
2023-10-28 04:24:23 +0600

    блок с абонементами стилизован

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,10 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
-
+import { iosVhFix } from "./utils/ios-vh-fix";
+import { initModals } from "./modules/modals/init-modals";
+import { Form } from "./modules/form-validate/form";
+import { initTabs } from "./tabs/init-tabs";
 // ---------------------------------
 
-window.addEventListener('DOMContentLoaded', () => {
-
+window.addEventListener("DOMContentLoaded", () => {
   // Utils
   // ---------------------------------
 
@@ -16,8 +15,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     initModals();
+    initTabs();
     const form = new Form();
     window.form = form;
     form.init();
