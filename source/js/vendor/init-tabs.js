cd5a16a89f0800f91e1c91cf509b9ef7d70f580d
tayga2410 <moldazhanov.eldar@gmail.com>
2023-11-04 19:31:48 +0600

    разбор состоянии в секции абонементы

--- source/js/vendor/init-tabs.js
+++ source/js/vendor/init-tabs.js
@@ -1,31 +1,39 @@
 function initTabs() {
-  const buttons = document.querySelectorAll('.subscription__tabs-controls button');
-  const prices = document.querySelectorAll('.subscription__price-wrapper > span:first-child');
-  const fadedPrices = document.querySelectorAll('.subscription__price-wrapper > span:nth-child(2)');
+  const buttons = document.querySelectorAll(".subscription__tabs-controls button");
+  const prices = document.querySelectorAll("[data-content]");
 
   const pricesPerPeriod = [
     [5000, 1700, 2700],
     [30000, 10200, 16200],
-    [60000, 20400, 32400]
+    [60000, 20400, 32400],
   ];
 
-  buttons.forEach((button, index) => {
-    button.addEventListener('click', function() {
-      prices.forEach((price, idx) => {
-        price.textContent = pricesPerPeriod[index][idx];
-      });
+  function updatePrices(index) {
+    prices.forEach((price, idx) => {
+      const currentPrice = pricesPerPeriod[index][idx];
+      const icon = price.querySelector('svg'); 
 
-      fadedPrices.forEach((fadedPrice, idx) => {
-        fadedPrice.textContent = pricesPerPeriod[index][idx];
-      });
+      price.setAttribute('data-content', currentPrice); 
+      price.textContent = ''; 
+      price.appendChild(document.createTextNode(currentPrice)); 
+      price.appendChild(icon); 
+    });
+  }
+
+  updatePrices(0);
+ 
+  buttons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      updatePrices(index); 
 
       buttons.forEach((btn) => {
-        btn.classList.remove('subscription__button--is-selected');
+        btn.classList.remove("subscription__tabs-control--is-selected");
       });
 
-      button.classList.add('subscription__button--is-selected');
+      button.classList.add("subscription__tabs-control--is-selected");
     });
   });
+
 }
 
 export default initTabs;
