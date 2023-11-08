ecbbcd76586899e15b2a438a404abc0aef211b62
tayga2410 <moldazhanov.eldar@gmail.com>
2023-11-08 18:51:44 +0600

    состояния кнопок на табах и аккордеоне
    Попытка сделать переход

--- source/js/vendor/init-accordion.js
+++ source/js/vendor/init-accordion.js
@@ -1,13 +1,19 @@
 function initAccordion() {
   const tabControls = document.querySelectorAll(".faq__tab-control");
-  const accordionWrappers = document.querySelectorAll(".faq__accordion-wrapper");
-
-  accordionWrappers.forEach((accordion) => {
-    accordion.style.display = "none";
-  });
-
-  accordionWrappers[0].style.display = "block";
+  const accordionWrappers = document.querySelectorAll(
+    ".faq__accordion-wrapper"
+  );
   tabControls[0].classList.add("faq__tab-control--is-selected");
+
+  const firstAccordionItem = document.querySelector(
+    "li[data-target='accordion']"
+  );
+
+  if (firstAccordionItem) {
+    firstAccordionItem.classList.add("is-opened");
+    const content = firstAccordionItem.querySelector("p");
+    content.style.display = "block";
+  }
 
   tabControls.forEach((control) => {
     control.addEventListener("click", () => {
@@ -18,35 +24,113 @@ function initAccordion() {
       control.classList.add("faq__tab-control--is-selected");
 
       const target = control.getAttribute("data-target");
-      const accordion = document.querySelector(`.faq__accordion-wrapper[data-target="${target}"]`);
+      const accordion = document.querySelector(
+        `.faq__accordion-wrapper[data-target="${target}"]`
+      );
+   
+      accordionWrappers.forEach((accWrapper) => {
+        accWrapper.classList.remove("is-active");
 
-      accordionWrappers.forEach((accordion) => {
-        accordion.style.display = "none";
-      });
-
-      accordion.style.display = "block";
+         accordion.classList.add("is-active");
+    });
     });
   });
 
-  const accordionButtons = document.querySelectorAll(".faq__accordion-wrapper .faq__button");
+  const accordionItems = document.querySelectorAll(
+    "li[data-target='accordion']"
+  );
 
-  accordionButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const content = button.nextElementSibling;
+  accordionItems.forEach((item) => {
+    const button = item.querySelector(".faq__button");
 
-      if (button.classList.contains("is-opened")) {
+    item.addEventListener("click", () => {
+      const isOpened = item.classList.contains("is-opened");
+
+      accordionItems.forEach((accordion) => {
+        const content = accordion.querySelector("p");
+        const accButton = accordion.querySelector(".faq__button");
+
+        accordion.classList.remove("is-opened");
+        accButton.classList.remove("is-opened");
+        accButton.classList.add("is-closed");
+        content.style.display = "none";
+      });
+
+      if (!isOpened) {
+        item.classList.add("is-opened");
+        button.classList.add("is-opened");
+        button.classList.remove("is-closed");
+        const content = item.querySelector("p");
+        content.style.display = "block";
+      } else {
+        item.classList.remove("is-opened");
         button.classList.remove("is-opened");
         button.classList.add("is-closed");
+        const content = item.querySelector("p");
         content.style.display = "none";
-      } else if (button.classList.contains("is-closed")) {
-        button.classList.remove("is-closed");
-        content.style.display = "block";
-        button.classList.add("is-opened");
-      } else {
-        button.classList.add("is-opened");
-        content.style.display = "block";
       }
     });
+  });
+
+  accordionItems.forEach((item) => {
+    item.addEventListener("keydown", handleAccordionFocus);
+  });
+
+  const handleAccordionFocus = (e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      e.preventDefault();
+      const isOpened = e.currentTarget.classList.contains("is-opened");
+
+      accordionItems.forEach((accordion) => {
+        const content = accordion.querySelector("p");
+
+        accordion.classList.remove("is-opened");
+        content.style.display = "none";
+      });
+
+      if (!isOpened) {
+        e.currentTarget.classList.add("is-opened");
+        const content = e.currentTarget.querySelector("p");
+        content.style.display = "block";
+      } else {
+        e.currentTarget.classList.remove("is-opened");
+        const content = e.currentTarget.querySelector("p");
+        content.style.display = "none";
+      }
+    }
+  };
+
+  accordionItems.forEach((item) => {
+    item.addEventListener("keydown", handleAccordionFocus);
+  });
+  
+  const handleTabFocus = (e) => {
+    const currentIndex = Array.from(tabControls).findIndex((tab) =>
+      tab.classList.contains("faq__tab-control--is-selected")
+    );
+
+    if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+      e.preventDefault();
+
+      if (e.key === "ArrowUp") {
+        const previousIndex =
+          (currentIndex - 1 + tabControls.length) % tabControls.length;
+        tabControls[previousIndex].click();
+        tabControls[previousIndex].focus();
+      } else if (e.key === "ArrowDown") {
+        const nextIndex = (currentIndex + 1) % tabControls.length;
+        tabControls[nextIndex].click();
+        tabControls[nextIndex].focus();
+      }
+    }
+  };
+
+  tabControls.forEach((tab, index) => {
+    tab.addEventListener("keydown", handleTabFocus);
+  });
+
+  tabControls.forEach((tab) => {
+    tab.addEventListener("keydown", handleTabFocus);
   });
 }
 
